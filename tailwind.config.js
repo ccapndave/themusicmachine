6ab6ac43d14976a051e7fe3029a43b224db23e8f
Dave Keen <dev@ruffness.com>
2021-04-21 07:33:53 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./_includes/**/*.html", "./_layouts/**/*.html", "./*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        title: "Source Code Pro",
+        body: "Poppins",
+      },
+      colors: {
+        yellow: {
+          "900-1/2": "hsla(22, 78%, 26%, 0.5)",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
