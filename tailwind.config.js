733b368e6380850f5a54dde8a3651f0afdcdb71e
ccapndave <ccapndave@users.noreply.github.com>
2022-07-14 17:24:34 +0000

    jekyll build from Action a993766bf5fb1c26a7129c68c43527372bef76d1

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_pages/**/*.html",
+    "./_data/**/*.yml",
+    "./*.html",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        title: "Source Code Pro",
+        body: "Poppins",
+      },
+      colors: {
+        yellow: {
+          "900-1/2": "hsla(22, 78%, 26%, 0.5)",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
